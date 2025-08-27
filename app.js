a0ad49b072752a4f8f9fc604d052615eba92fcf9
Arian Grasso <ariangrasso@gmail.com>
2025-08-27 00:08:21 -0300

    guardado en variables

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+let recital = JSON.parse(localStorage.getItem("recitales")) || [];
+let idRecital = 1;
 
+const form = document.getElementById("formularioRecitales");
+const lista = document.getElementById("listaRecitales");
+
+form.addEventListener("submit", () => {
+    const artista = document.getElementById("artista").value;
+    const precio = document.getElementById("precio").value;
+    const sector = document.getElementById("sector").value;
+    const fecha = document.getElementById("fecha").value;
+});
